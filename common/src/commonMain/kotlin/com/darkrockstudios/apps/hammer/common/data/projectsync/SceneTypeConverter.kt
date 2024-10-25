08d60a5f434355b1cb452a157a14ba95e258a177
Adam Brown <adamwbrown@gmail.com>
2024-10-24 22:17:38 -0700

    Fixed scene syncing markForSync must be called BEFORE the mutation

--- common/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/data/projectsync/SceneTypeConverter.kt
+++ common/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/data/projectsync/SceneTypeConverter.kt
@@ -1,6 +1,8 @@
 package com.darkrockstudios.apps.hammer.common.data.projectsync
 
+import com.darkrockstudios.apps.hammer.base.http.ApiProjectEntity
 import com.darkrockstudios.apps.hammer.base.http.ApiSceneType
+import com.darkrockstudios.apps.hammer.base.http.EntityType
 import com.darkrockstudios.apps.hammer.common.data.SceneItem
 
 fun SceneItem.Type.toApiType(): ApiSceneType {
@@ -16,5 +18,15 @@ fun ApiSceneType.toSceneType(): SceneItem.Type {
 	return when (this) {
 		ApiSceneType.Scene -> SceneItem.Type.Scene
 		ApiSceneType.Group -> SceneItem.Type.Group
+	}
+}
+
+fun ApiProjectEntity.Type.toEntityType(): EntityType {
+	return when (this) {
+		ApiProjectEntity.Type.SCENE -> EntityType.Scene
+		ApiProjectEntity.Type.NOTE -> EntityType.Note
+		ApiProjectEntity.Type.TIMELINE_EVENT -> EntityType.TimelineEvent
+		ApiProjectEntity.Type.ENCYCLOPEDIA_ENTRY -> EntityType.EncyclopediaEntry
+		ApiProjectEntity.Type.SCENE_DRAFT -> EntityType.SceneDraft
 	}
 }
