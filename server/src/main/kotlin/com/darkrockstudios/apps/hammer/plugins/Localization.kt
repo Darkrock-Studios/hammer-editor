a13774d7424dda802f16b05ee6f4f524417d13ff
Adam Brown <adamwbrown@gmail.com>
2025-03-30 22:47:26 -0700

    Set default locale to English

--- server/src/main/kotlin/com/darkrockstudios/apps/hammer/plugins/Localization.kt
+++ server/src/main/kotlin/com/darkrockstudios/apps/hammer/plugins/Localization.kt
@@ -3,10 +3,12 @@ package com.darkrockstudios.apps.hammer.plugins
 import com.darkrockstudios.apps.hammer.utilities.ResUtils
 import com.github.aymanizz.ktori18n.I18n
 import io.ktor.server.application.*
+import java.util.*
 
 fun Application.configureLocalization() {
 	install(I18n) {
 		supportedLocales = ResUtils.getTranslatedLocales()
+		defaultLocale = Locale.ENGLISH
 		//useOfCookie = true
 		//useOfRedirection = true
 		//excludePrefixes("/api")
