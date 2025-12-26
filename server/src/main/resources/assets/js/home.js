1e9ed1cb90ea209a08031ba5f4fe01b419b8a70f
Adam Brown <adamwbrown@gmail.com>
2025-12-26 00:27:46 -0800

    Converting more to download links to drop downs

--- server/src/main/resources/assets/js/home.js
+++ server/src/main/resources/assets/js/home.js
@@ -1,14 +1,20 @@
-// Linux download format selector
+// Download format selectors for Windows, macOS, and Linux
 (function () {
-	const select = document.getElementById('linux-format-select');
-	const btn = document.getElementById('linux-download-btn');
-	const icon = document.getElementById('linux-download-icon');
+	function setupDownloadSelector(selectId, btnId, iconId) {
+		const select = document.getElementById(selectId);
+		const btn = document.getElementById(btnId);
+		const icon = document.getElementById(iconId);
 
-	if (select && btn && icon) {
-		select.addEventListener('change', function () {
-			const selected = this.options[this.selectedIndex];
-			btn.href = selected.dataset.url;
-			icon.className = selected.dataset.icon;
-		});
+		if (select && btn && icon) {
+			select.addEventListener('change', function () {
+				const selected = this.options[this.selectedIndex];
+				btn.href = selected.dataset.url;
+				icon.className = selected.dataset.icon;
+			});
+		}
 	}
+
+	setupDownloadSelector('windows-format-select', 'windows-download-btn', 'windows-download-icon');
+	setupDownloadSelector('macos-format-select', 'macos-download-btn', 'macos-download-icon');
+	setupDownloadSelector('linux-format-select', 'linux-download-btn', 'linux-download-icon');
 })();
