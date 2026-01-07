4554655345c93ddf7aaf7d6142ea15d64ad2c6ba
Adam Brown <adamwbrown@gmail.com>
2026-01-06 19:01:52 -0800

    Add client version check to about

--- base/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/base/community.kt
+++ base/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/base/community.kt
@@ -3,3 +3,4 @@ package com.darkrockstudios.apps.hammer.base
 const val GITHUB_URL = "https://github.com/Wavesonics/hammer-editor/"
 const val DISCORD_URL = "https://discord.gg/GTmgjZcupk"
 const val REDDIT_URL = "https://www.reddit.com/r/hammereditor/"
+const val VERSION_CHECK_URL = "https://api.github.com/repos/Wavesonics/hammer-editor/releases/latest"
