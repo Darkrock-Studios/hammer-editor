e8ff43b8c372059f915d898de12958b002b126f2
Adam Brown <adamwbrown@gmail.com>
2026-01-08 21:04:25 -0800

    Increase Argon2 parameters

--- server/src/main/kotlin/com/darkrockstudios/apps/hammer/account/AccountsRepository.kt
+++ server/src/main/kotlin/com/darkrockstudios/apps/hammer/account/AccountsRepository.kt
@@ -210,8 +210,8 @@ class AccountsRepository(
 
 		// Argon2 parameters
 		const val ARGON2_MEMORY_COST_KIB = 65536  // 64 MiB
-		const val ARGON2_TIME_COST = 2  // iterations
-		const val ARGON2_PARALLELISM = 1  // threads
+		const val ARGON2_TIME_COST = 3  // iterations
+		const val ARGON2_PARALLELISM = 2  // threads
 
 		// TODO: (?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])
 		private val emailPattern = Regex("^[A-Za-z0-9+_.-]+@(.+)$")
