02b9ca60210f753605d3315d20b0055e45e87b0a
Wavesonics <adamwbrown@gmail.com>
2026-01-10 20:31:40 -0800

    Flatpak work

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
-import com.darkrockstudios.build.registerLinuxDistributionTasks
-import com.darkrockstudios.build.registerPrepareForReleaseTask
-import com.darkrockstudios.build.registerPublishTasks
+import com.darkrockstudios.build.*
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 
 group = "com.darkrockstudios.apps.hammer"
@@ -86,6 +84,82 @@ kover {
 	}
 }
 
-registerPrepareForReleaseTask()
 registerPublishTasks()
 registerLinuxDistributionTasks(libs.versions.app.get())
+
+tasks.register("prepareForRelease") {
+	doLast {
+		val releaseInfo =
+			configureRelease(libs.versions.app.get()) ?: error("Failed to configure new release")
+
+		println("Creating new release")
+		val versionCode = releaseInfo.semVar.createVersionCode(true, 0)
+
+		// Write the new version number
+		val versionsPath = "gradle/libs.versions.toml".replace("/", File.separator)
+		val versionsFile = project.rootDir.resolve(versionsPath)
+		writeSemvar(libs.versions.app.get(), releaseInfo.semVar, versionsFile)
+
+		// Google Play has a hard limit of 500 characters
+		val truncatedChangelog = if (releaseInfo.changeLog.length > 500) {
+			"${releaseInfo.changeLog.take(480)}... and more"
+		} else {
+			releaseInfo.changeLog
+		}
+
+		// Write the Fastlane changelog file
+		val rootDir: File = project.rootDir
+		val changelogsPath =
+			"fastlane/metadata/android/en-US/changelogs".replace("/", File.separator)
+		val changeLogsDir = rootDir.resolve(changelogsPath)
+		val changeLogFile = File(changeLogsDir, "$versionCode.txt")
+		changeLogFile.writeText(truncatedChangelog)
+		println("Changelog for version ${releaseInfo.semVar} written to $changelogsPath/$versionCode.txt")
+
+		// Write the Global changelog file
+		val globalChangelogFile = File("${project.rootDir}/CHANGELOG.md")
+		writeChangelogMarkdown(releaseInfo, globalChangelogFile)
+
+		// Update snapcraft.yaml with new version and JVM version
+		val snapcraftPath = "snap/snapcraft.yaml".replace("/", File.separator)
+		val snapcraftFile = project.rootDir.resolve(snapcraftPath)
+		val jvmVersion = libs.versions.jvm.get()
+		updateSnapcraftYaml(releaseInfo.semVar, jvmVersion, snapcraftFile)
+
+		// Update Flatpak manifest and metainfo with new version and JVM version
+		val flatpakManifestPath = "flatpak/com.darkrockstudios.hammer.yaml".replace("/", File.separator)
+		val flatpakManifestFile = project.rootDir.resolve(flatpakManifestPath)
+		val flatpakMetainfoPath = "flatpak/com.darkrockstudios.hammer.metainfo.xml".replace("/", File.separator)
+		val flatpakMetainfoFile = project.rootDir.resolve(flatpakMetainfoPath)
+		updateFlatpakFiles(releaseInfo.semVar, jvmVersion, flatpakManifestFile, flatpakMetainfoFile)
+
+		// Commit the changes to the repo
+		exec { commandLine = listOf("git", "add", changeLogFile.absolutePath) }
+		exec { commandLine = listOf("git", "add", versionsFile.absolutePath) }
+		exec { commandLine = listOf("git", "add", globalChangelogFile.absolutePath) }
+		exec { commandLine = listOf("git", "add", snapcraftFile.absolutePath) }
+		exec { commandLine = listOf("git", "add", flatpakManifestFile.absolutePath) }
+		exec { commandLine = listOf("git", "add", flatpakMetainfoFile.absolutePath) }
+		exec {
+			commandLine =
+				listOf("git", "commit", "-m", "Prepared for release: v${releaseInfo.semVar}")
+		}
+
+		// Merge develop into release
+		exec { commandLine = listOf("git", "checkout", "release") }
+		exec { commandLine = listOf("git", "merge", "develop") }
+
+		// Create the release tag
+		exec {
+			commandLine =
+				listOf("git", "tag", "-a", "v${releaseInfo.semVar}", "-m", releaseInfo.changeLog)
+		}
+
+		// Push and begin the release process
+		exec { commandLine = listOf("git", "push", "origin", "--all") }
+		exec { commandLine = listOf("git", "push", "origin", "--tags") }
+
+		// Leave the repo back on develop
+		exec { commandLine = listOf("git", "checkout", "develop") }
+	}
+}
