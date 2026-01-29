c552fc6497dfc7673dfd8e180135cc4f4584b8ff
Wavesonics <adamwbrown@gmail.com>
2026-01-28 21:04:11 -0800

    Remove invalid prop

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -218,8 +218,6 @@ fun findMakeAppx(): String? {
 tasks.named("flatpakGradleGenerator") {
 	setProperty("outputFile", rootProject.file("flatpak/flatpak-sources.json"))
 
-	setProperty("localRepoPath", "offline-repository")
-
 	// Exclude test configurations to reduce the dependency list
 	setProperty("excludeConfigurations", listOf(
 		"testCompileClasspath",
