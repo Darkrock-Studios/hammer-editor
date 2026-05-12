591c3a3216db4e94589094e1baf71986f9271c87
Adam Brown <adamwbrown@gmail.com>
2026-05-12 00:53:22 -0700

    Fabricate writing activity when installing the example project

--- common/src/desktopMain/kotlin/com/darkrockstudios/apps/hammer/common/data/ExampleProjectRepositoryDesktop.kt
+++ common/src/desktopMain/kotlin/com/darkrockstudios/apps/hammer/common/data/ExampleProjectRepositoryDesktop.kt
@@ -3,12 +3,13 @@ package com.darkrockstudios.apps.hammer.common.data
 import com.darkrockstudios.apps.hammer.common.data.globalsettings.GlobalSettingsRepository
 import com.darkrockstudios.apps.hammer.common.util.zip.unzipBytesToDirectory
 import io.github.aakira.napier.Napier
+import net.peanuuutz.tomlkt.Toml
 import okio.FileSystem
-import okio.Path.Companion.toPath
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 import java.io.InputStream
+import kotlin.time.Clock
 
 
 actual val exampleProjectModule = module {
@@ -17,8 +18,10 @@ actual val exampleProjectModule = module {
 
 class ExampleProjectRepositoryDesktop(
 	globalSettingsRepository: GlobalSettingsRepository,
-	private val fileSystem: FileSystem
-) : ExampleProjectRepository(globalSettingsRepository) {
+	fileSystem: FileSystem,
+	toml: Toml,
+	clock: Clock,
+) : ExampleProjectRepository(globalSettingsRepository, fileSystem, toml, clock) {
 
 	private fun loadExampleProjectZip(): ByteArray {
 		val path = "/raw/$EXAMPLE_PROJECT_FILE_NAME"
@@ -46,11 +49,5 @@ class ExampleProjectRepositoryDesktop(
 		} else {
 			Napier.i("Skipping example project creation")
 		}
-	}
-
-	private fun projectsDir() = globalSettingsRepository.globalSettings.projectsDirectory.toPath()
-
-	companion object {
-		private const val PROJECT_NAME = "Alice In Wonderland"
 	}
 }
