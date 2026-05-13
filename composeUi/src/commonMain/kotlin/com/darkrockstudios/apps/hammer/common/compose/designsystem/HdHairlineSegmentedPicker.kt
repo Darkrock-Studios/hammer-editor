9aca839ffe1b97ac48f20f32df0ee97dae9e0c3d
Adam Brown <adamwbrown@gmail.com>
2026-05-13 00:13:11 -0700

    Segment picker now sizes better on small screens

--- composeUi/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/compose/designsystem/HdHairlineSegmentedPicker.kt
+++ composeUi/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/common/compose/designsystem/HdHairlineSegmentedPicker.kt
@@ -2,20 +2,14 @@ package com.darkrockstudios.apps.hammer.common.compose.designsystem
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.darkrockstudios.apps.hammer.common.compose.Ui
@@ -83,6 +77,9 @@ private fun HdSegmentedCell(
 			text = label.uppercase(),
 			style = MaterialTheme.typography.labelMedium,
 			color = labelColor,
+			maxLines = 1,
+			softWrap = false,
+			overflow = TextOverflow.Ellipsis,
 		)
 	}
 }
