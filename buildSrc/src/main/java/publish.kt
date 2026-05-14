cfa68ec0640ea1997382dcdfe170032447ad7e9a
Adam Brown <adamwbrown@gmail.com>
2026-05-14 01:30:16 -0700

    Move F-Droid tag creation from Gradle task into publish workflow

--- buildSrc/src/main/java/publish.kt
+++ buildSrc/src/main/java/publish.kt
@@ -2,41 +2,6 @@ package com.darkrockstudios.build
 
 import org.gradle.api.Project
 
-fun publishFdroid(project: Project) {
-	val libs = project.extensions.getByType(org.gradle.api.artifacts.VersionCatalogsExtension::class.java).named("libs")
-	val semvarStr = libs.findVersion("app").get().toString()
-	val curSemVar = parseSemVar(semvarStr)
-	val versionCode = curSemVar.createVersionCode(true, 0)
-	val tag = "fdroid-${versionCode}"
-
-	project.providers.exec {
-		commandLine = listOf(
-			"git",
-			"config",
-			"--global",
-			"user.email",
-			"github-actions[bot]@users.noreply.github.com"
-		)
-	}.result.get()
-	project.providers.exec {
-		commandLine = listOf("git", "config", "--global", "user.name", "github-actions[bot]")
-	}.result.get()
-
-	project.providers.exec { commandLine = listOf("git", "fetch", "origin", "release") }.result.get()
-	project.providers.exec { commandLine = listOf("git", "checkout", "release") }.result.get()
-	project.providers.exec {
-		commandLine = listOf(
-			"git",
-			"tag",
-			"-a",
-			tag,
-			"-m",
-			"FDroid release tag for $semvarStr"
-		)
-	}.result.get()
-	project.providers.exec { commandLine = listOf("git", "push", "origin", "tag", tag) }.result.get()
-}
-
 fun publishFlathub(project: Project) {
 	val libs = project.extensions.getByType(org.gradle.api.artifacts.VersionCatalogsExtension::class.java).named("libs")
 	val semvarStr = libs.findVersion("app").get().toString()
@@ -74,12 +39,6 @@ fun publishFlathub(project: Project) {
  * Registers the publish tasks for F-Droid and Flathub.
  */
 fun Project.registerPublishTasks() {
-	tasks.register("publishFdroid") {
-		doLast {
-			publishFdroid(project)
-		}
-	}
-
 	tasks.register("publishFlathub") {
 		doLast {
 			publishFlathub(project)
