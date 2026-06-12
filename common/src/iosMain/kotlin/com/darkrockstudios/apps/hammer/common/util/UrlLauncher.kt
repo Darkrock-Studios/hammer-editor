a5188760e2116d23e2b3ea1873b97ffdcea5f30c
Wavesonics <adamwbrown@gmail.com>
2026-06-11 22:16:20 -0700

    Implement iOS stubs: URL launcher, date formatting, network reachability, backup share
    - UrlLauncher: open URLs via UIApplication.openURL (fixes update dialog button)
    - DateTimeUtils: format Instant/LocalDateTime properly (was empty / used now())
    - NetworkConnectivity: real reachability via NWPathMonitor with 2s timeout
    - BackupManagerService: present iOS share sheet via UIActivityViewController
    - FocusModeService: documented as noop (iOS has no public DND API)

--- common/src/iosMain/kotlin/com/darkrockstudios/apps/hammer/common/util/UrlLauncher.kt
+++ common/src/iosMain/kotlin/com/darkrockstudios/apps/hammer/common/util/UrlLauncher.kt
@@ -1,7 +1,15 @@
 package com.darkrockstudios.apps.hammer.common.util
 
+import platform.Foundation.NSURL
+import platform.UIKit.UIApplication
+
 class UrlLauncherDarwin : UrlLauncher {
 	override fun openInBrowser(url: String) {
-		// TODO ios implementation needed
+		val nsUrl = NSURL.URLWithString(url) ?: return
+		UIApplication.sharedApplication.openURL(
+			url = nsUrl,
+			options = emptyMap<Any?, Any>(),
+			completionHandler = null,
+		)
 	}
 }
