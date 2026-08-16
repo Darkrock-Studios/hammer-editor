f0372c885d46a04b7ccd7432b5e6fbf788fcd68b
Adam Brown <adamwbrown@gmail.com>
2026-08-15 21:53:38 -0700

    Update discord links

--- base/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/base/community.kt
+++ base/src/commonMain/kotlin/com/darkrockstudios/apps/hammer/base/community.kt
@@ -2,7 +2,7 @@ package com.darkrockstudios.apps.hammer.base
 
 const val GITHUB_URL = "https://github.com/Darkrock-Studios/hammer-editor/"
 const val RELEASES_LATEST_URL = "${GITHUB_URL}releases/latest"
-const val DISCORD_URL = "https://discord.gg/GTmgjZcupk"
+const val DISCORD_URL = "https://discord.gg/49Kj5mMj6d"
 const val REDDIT_URL = "https://www.reddit.com/r/hammereditor/"
 const val VERSION_CHECK_URL = "https://api.github.com/repos/Darkrock-Studios/hammer-editor/releases/latest"
 const val HAMMER_INK_URL = "https://hammer.ink/"
